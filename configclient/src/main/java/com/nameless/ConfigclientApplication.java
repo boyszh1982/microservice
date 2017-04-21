ddd26c5ee8e4607fe62eafbc01bf428a197154fc
boyszh1982@163.com <boyszh1982@163.com>
2017-04-21 22:27:02 +0800

    update

--- configclient/src/main/java/com/nameless/ConfigclientApplication.java
+++ configclient/src/main/java/com/nameless/ConfigclientApplication.java
@@ -6,6 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfigclientApplication {
 
+	/**
+	 * rabbitmqctl.bat environment --查看端口等
+	 * rabbitmqctl.bat list_users --查看用户
+	 * 
+	 * bus 不起作用，没有通知其他节点conf的变更，连自己的脚本都刷新不到了。
+	 * 
+	 * http://localhost:7001/configclient/uat
+	 * http://localhost:8080/say $curl -d {} localhost:8080/refresh
+	 * http://localhost:8090/say 不生效 why?
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigclientApplication.class, args);
 	}
--- configserver/src/main/java/com/nameless/ConfigserverApplication.java
+++ configserver/src/main/java/com/nameless/ConfigserverApplication.java
@@ -12,6 +12,10 @@ public class ConfigserverApplication {
 	 * configserver在GITHUB上脚本变更后会自动刷新,
 	 * 此时需要对configcleint执行 curl -d {} http://localhost:8080/refresh刷新配置
 	 * 需要用户名密码的注册中心，如何做？
+	 * configserver高可用如何实现？
+	 * 断路器
+	 * 服务发现
+	 * 消息队列-红包在内存中生成
 	 * @param args
 	 */
 	public static void main(String[] args) {
